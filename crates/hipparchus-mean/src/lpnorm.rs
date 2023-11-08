1ad0d115f5fc4d0a5965e9f19c75a6f6be6979ab
Guang Yang <114556848+web3nemo@users.noreply.github.com>
2023-11-08 13:14:16 +0800

    Geo (#21)
    * zone and more unit test
    
    * metrics for set and text
    
    * stats distance
    
    * stats distance
    
    * refactor and fix bugs
    
    ---------
    
    Co-authored-by: Guang Yang <guangya@microsoft.com>

--- crates/hipparchus-mean/src/lpnorm.rs
+++ crates/hipparchus-mean/src/lpnorm.rs
@@ -10,17 +10,17 @@ where
     /// Compute the Lp norm of a vector with the specified `p` prameter (p=0, 1, 2, ..., Inf)
     fn lpnorm(self, p:f32) -> Option<T>;
 
-    /// Compute the L0 norm of a vector
-    fn l0norm(self) -> Option<T>;
-
-    /// Compute the L1 norm of a vector
-    fn l1norm(self) -> Option<T>;
+    /// Compute the Lp norm of a vector with p = Inf
+    fn lpnorm_inf(self) -> Option<T>;
 
     /// Compute the L2 norm of a vector
     fn l2norm(self) -> Option<T>;
 
-    /// Compute the Lp norm of a vector with p = Inf
-    fn lpnorm_inf(self) -> Option<T>;
+    /// Compute the L1 norm of a vector
+    fn l1norm(self) -> Option<T>;
+
+    /// Compute the L0 norm of a vector
+    fn l0norm(self) -> Option<T>;
 }
 
 impl<'a,T,I> LpNorm<'a,T> for I
@@ -32,47 +32,34 @@ where
     {
         match p
         {
-            p if approx_eq!(f32, p, 0.0) => self.l0norm(),
-            p if approx_eq!(f32, p, 1.0) => self.l1norm(),
+            p if p.is_infinite() && p.is_sign_positive() => self.lpnorm_inf(),
             p if approx_eq!(f32, p, 2.0) => self.l2norm(),
-            p if p.is_infinite() => self.lpnorm_inf(),
-            _ =>
+            p if approx_eq!(f32, p, 1.0) => self.l1norm(),
+            p if approx_eq!(f32, p, 0.0) => self.l0norm(),
+            p if p >= 0.0 => 
             {
-                let k = T::from_f32(p).unwrap();
+                let p = T::from_f32(p).unwrap();
                 let mut empty = true;
                 let sum = self.fold(T::zero(), |s,&x|
                 {
                     empty = false;
-                    s + x.powf(k)
+                    s + x.abs().powf(p)
                 });
     
-                if empty { None } else { Some(sum.powf(T::one()/k)) }
+                if empty { None } else { Some(sum.powf(T::one()/p)) }
             }
+            _ => None
         }
     }
 
-    fn l0norm(self) -> Option<T>
+    fn lpnorm_inf(self) -> Option<T>
     {
-        let mut empty = true;
-        let sum = self.fold(0usize, |s,&x|
+        let agg = self.max_by(|a, b| a.partial_cmp(b).unwrap());
+        match agg
         {
-            empty = false;
-            if T::zero() != x { s + 1 } else { s }
-        });
-        
-        if empty { None } else { T::from_usize(sum) }
-    }
-
-    fn l1norm(self) -> Option<T>
-    {
-        let mut empty = true;
-        let sum = self.fold(T::zero(), |s,&x|
-        {
-            empty = false;
-            s + x
-        });
-
-        if empty { None } else { Some(sum) }
+            None => None,
+            _ => Some(*(agg.unwrap()))
+        }
     }
 
     fn l2norm(self) -> Option<T>
@@ -87,14 +74,28 @@ where
         if empty { None } else { Some(sum.sqrt()) }
     }
 
-    fn lpnorm_inf(self) -> Option<T>
+    fn l1norm(self) -> Option<T>
     {
-        let agg = self.max_by(|a, b| a.partial_cmp(b).unwrap());
-        match agg
+        let mut empty = true;
+        let sum = self.fold(T::zero(), |s,&x|
         {
-            None => None,
-            _ => Some(*(agg.unwrap()))
-        }
+            empty = false;
+            s + x.abs()
+        });
+
+        if empty { None } else { Some(sum) }
+    }
+
+    fn l0norm(self) -> Option<T>
+    {
+        let mut empty = true;
+        let sum = self.fold(0usize, |s,&x|
+        {
+            empty = false;
+            if x.is_zero() { s } else { s + 1 }
+        });
+        
+        if empty { None } else { T::from_usize(sum) }
     }
 }
 
@@ -102,71 +103,38 @@ where
 mod tests 
 {
     use super::*;
+    use rstest::*;
     use float_cmp::assert_approx_eq;
 
-    #[test]
-    fn test_l0norm()
+    #[rstest]
+    #[case(vec![0.0, 1.0, 2.0, 3.0, 4.0], 0.0, 4.0)]
+    #[case(vec![1.0, 2.0, 3.0, 4.0, 5.0], 1.0, 15.0)]
+    #[case(vec![3.0, 4.0], 2.0, 5.0)]
+    #[case(vec![1.0, 2.0, 3.0, 4.0, 5.0], f32::INFINITY, 5.0)]
+    #[case(vec![1.0, 1.0, 1.0, 2.0, 2.0, 2.0], 3.0, 3.0)]
+    fn test_lpnorm(#[case] v: Vec<f32>, #[case] p: f32, #[case] expected: f32)
     {
-        let v = vec![0.0, 1.0, 2.0, 3.0, 4.0];
-        assert_approx_eq!
-        (
-            f32, 4.0,
-            v.iter().lpnorm(0.0).unwrap()
-        );
+        assert_approx_eq!(f32, expected, v.iter().lpnorm(p).unwrap());
     }
 
-    #[test]
-    fn test_l1norm()
-    {
-        let v = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-        assert_approx_eq!
-        (
-            f32, 15.0,
-            v.iter().lpnorm(1.0).unwrap()
-        );
-    }
-
-    #[test]
-    fn test_l2norm()
-    {
-        let v = vec![3.0, 4.0];
-        assert_approx_eq!
-        (
-            f32, 5.0,
-            v.iter().lpnorm(2.0).unwrap()
-        );
-    }    // Test Lp norm 
-
-    #[test]
-    fn test_lpnorm_inf()
-    {
-        let v = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-        assert_approx_eq!
-        (
-            f32, 5.0,
-            v.iter().lpnorm(f32::INFINITY).unwrap()
-        );
-    }
-
-    #[test]
-    fn test_lpnorm()
-    {
-        let v = vec![1.0, 1.0, 1.0, 2.0, 2.0, 2.0];
-        assert_approx_eq!
-        (
-            f32, 3.0,
-            v.iter().lpnorm(3.0).unwrap()
-        );
-    }
-
-    #[test]
-    fn test_lpnorm_empty()
+    #[rstest]
+    #[case(0.0)]
+    #[case(1.0)]
+    #[case(2.0)]
+    #[case(3.0)]
+    #[case(f32::INFINITY)]
+    fn test_lpnorm_empty(#[case] p: f32,)
     {
         let e = vec![] as Vec<f32>;
-        assert_eq!(None, e.iter().l0norm());
-        assert_eq!(None, e.iter().l1norm());
-        assert_eq!(None, e.iter().l2norm());
-        assert_eq!(None, e.iter().lpnorm_inf());
-        assert_eq!(None, e.iter().lpnorm(3.0));
+        assert_eq!(None, e.iter().lpnorm(p));
+    }
+
+    #[rstest]
+    #[case(-1.0)]
+    #[case(f32::NEG_INFINITY)]
+    fn test_lpnorm_invalid(#[case] p: f32)
+    {
+        let v = vec![3.0, 4.0];
+        assert_eq!(None, v.iter().lpnorm(p));
     }
 }
