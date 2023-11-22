29b1c65de7a009035e1849ad051e8c6d85de3580
Guang Yang <114556848+web3nemo@users.noreply.github.com>
2023-11-22 12:31:43 +0800

    Inverse Trigonometric

--- crates/hipparchus-az/src/azimuth_trig.rs
+++ crates/hipparchus-az/src/azimuth_trig.rs
@@ -1,5 +1,5 @@
 use hipparchus_mean::Fp;
-use crate::{Trigonometric, Azimuth};
+use crate::{Azimuth, Trigonometric, InverseTrigonometric};
 
 impl<T> Trigonometric<T> for Azimuth<T> where T:Fp
 {   
@@ -20,6 +20,21 @@ impl<T> Trigonometric<T> for Azimuth<T> where T:Fp
     }
 }
 
+impl<T> InverseTrigonometric<T> for Azimuth<T> where T:Fp
+{
+    /// Returns the arc sine of the angle
+    fn asin(v:T) -> Self { Self::with_radians(v.asin()) }
+
+    /// Returns the arc cosine of the angle
+    fn acos(v:T) -> Self { Self::with_radians(v.acos()) }
+
+    /// Returns the arc tangent of the angle, where value is in the range [-PI/2, PI/2] radians
+    fn atan(v:T) -> Self { Self::with_radians(v.atan()) }
+
+    /// Returns the arc tangent of the angle, where value is in the range [-PI, PI) radians
+    fn atan2(y:T, x:T) -> Self { Self::with_radians(T::atan2(y, x)) }
+}
+
 #[cfg(test)]
 mod tests 
 {
@@ -33,15 +48,34 @@ mod tests
     {
         let a = Azimuth::new(y, x);
         let tan = a.tan();
+        let cot = a.cot();
+        assert_approx_eq!(f64, y/x, tan);
+        assert_approx_eq!(f64, x/y, cot);
+        assert_approx_eq!(f64, a.radians().tan(), tan);
+        assert_approx_eq!(f64, a.radians().cot(), cot);
         let sin = a.sin();
         let cos = a.cos();
-        assert_approx_eq!(f64, y/x, tan);
         assert_approx_eq!(f64, y/h, sin);
         assert_approx_eq!(f64, x/h, cos);
-        assert_approx_eq!(f64, a.radians().tan(), tan);
+        assert_approx_eq!(f64, tan, sin/cos);
+        assert_approx_eq!(f64, cot, cos/sin);
+        assert_approx_eq!(f64, 1.0, tan * cot);
+    }
+
+    #[rstest]
+    #[case(3.0, 4.0, 5.0)]
+    fn test_azimuth_trig_sincos(#[case] y: f64, #[case] x: f64, #[case] h: f64)
+    {
+        let a = Azimuth::new(y, x);
+        let sin = a.sin();
+        let cos = a.cos();
+        let (s, c) = a.sincos();
+        assert_approx_eq!(f64, sin, s);
+        assert_approx_eq!(f64, cos, c);
+        assert_approx_eq!(f64, y/h, sin);
+        assert_approx_eq!(f64, x/h, cos);
         assert_approx_eq!(f64, a.radians().sin(), sin);
         assert_approx_eq!(f64, a.radians().cos(), cos);
-        assert_approx_eq!(f64, tan, sin/cos);
         assert_approx_eq!(f64, 1.0, sin * sin + cos * cos);
     }
 
@@ -50,16 +84,32 @@ mod tests
     fn test_azimuth_sin(#[case] y: f64, #[case] x: f64, #[case] h: f64)
     {
         let a = Azimuth::new(y, x);
-        let tan = a.tan();
         let sin = a.sin();
-        let cos = a.cos();
-        assert_approx_eq!(f64, y/x, tan);
+        let sec = a.sec();
+        let vcos = a.vcos();
         assert_approx_eq!(f64, y/h, sin);
-        assert_approx_eq!(f64, x/h, cos);
-        assert_approx_eq!(f64, a.radians().tan(), tan);
+        assert_approx_eq!(f64, h/y, sec);
+        assert_approx_eq!(f64, 1.0, sec * sin);
+        assert_approx_eq!(f64, 1.0, vcos + sin);
         assert_approx_eq!(f64, a.radians().sin(), sin);
+        assert_approx_eq!(f64, a.radians().sec(), sec);
+        assert_approx_eq!(f64, a.radians().vcos(), vcos);
+    }
+
+    #[rstest]
+    #[case(3.0, 4.0, 5.0)]
+    fn test_azimuth_cos(#[case] y: f64, #[case] x: f64, #[case] h: f64)
+    {
+        let a = Azimuth::new(y, x);
+        let cos = a.cos();
+        let csc = a.csc();
+        let vsin = a.vsin();
+        assert_approx_eq!(f64, x/h, cos);
+        assert_approx_eq!(f64, h/x, csc);
+        assert_approx_eq!(f64, 1.0, csc * cos);
+        assert_approx_eq!(f64, 1.0, vsin + cos);
         assert_approx_eq!(f64, a.radians().cos(), cos);
-        assert_approx_eq!(f64, tan, sin/cos);
-        assert_approx_eq!(f64, 1.0, sin * sin + cos * cos);
+        assert_approx_eq!(f64, a.radians().csc(), csc);
+        assert_approx_eq!(f64, a.radians().vsin(), vsin);
     }
 }
--- crates/hipparchus-az/src/trig.rs
+++ crates/hipparchus-az/src/trig.rs
@@ -60,48 +60,56 @@ impl<T> Trigonometric<T> for T where T: Fp
 }
 
 /// Trait for inverse trigonometric functions
-pub trait InverseTrigonometric
+pub trait InverseTrigonometric<T> where T: Fp, Self: Sized
 {
-    type Output;
-
     /// Returns the arc sine of the angle
-    fn asin(&self) -> Self::Output;
+    fn asin(v:T) -> Self;
 
     /// Returns the arc cosine of the angle
-    fn acos(&self) -> Self::Output;
+    fn acos(v:T) -> Self;
 
     /// Returns the arc tangent of the angle, where value is in the range [-PI/2, PI/2] radians
-    fn atan(&self) -> Self::Output;
+    fn atan(v:T) -> Self;
 
     /// Returns the arc tangent of the angle, where value is in the range [-PI, PI) radians
-    fn atan2(y:(&Self, &Self)) -> Self::Output;
+    fn atan2(y:T, x:T) -> Self;
+
+    /// Returns the cotangent of the angle
+    fn acot(v:T) -> Self { Self::atan(v.recip()) }
+    
+    /// Returns the cotangent of the angle
+    #[inline]
+    fn acot2(y:T, x:T) -> Self { Self::atan2(x, y) }
+    
+    /// Return the secant of the angle
+    #[inline]
+    fn asec(v:T) -> Self { Self::asin(v.recip()) }
+
+    /// Return the cosecant of the angle
+    #[inline]
+    fn acsc(v:T) -> Self { Self::acos(v.recip()) }
+
+    /// Return the versine of the angle
+    #[inline]
+    fn avsin(v:T) -> Self { Self::acos(T::one() - v) }
+
+    /// Return the vercosine of the angle
+    #[inline]
+    fn avcos(v:T) -> Self { Self::asin(T::one() - v) }
 }
 
-impl<T> InverseTrigonometric for T where T: Fp
+impl<T> InverseTrigonometric<T> for T where T: Fp
 {
-    type Output = T;
-
-    fn asin(&self) -> Self::Output
-    {
-        T::asin(*self)
-    }
+    fn asin(v:T) -> Self { v.asin() }
     
-    fn acos(&self) -> Self::Output
-    {
-        T::acos(*self)
-    }
+    fn acos(v:T) -> Self { v.acos() }
 
-    fn atan(&self) -> Self::Output
-    {
-        T::atan(*self)
-    }
+    fn atan(v:T) -> Self { v.atan() }
     
-    fn atan2(y:(&Self, &Self)) -> Self::Output
-    {
-        T::atan2(*y.0, *y.1)
-    }
+    fn atan2(y:T, x:T) -> Self { T::atan2(y, x) }
 }
 
+/// Trait for hyperbolic functions
 pub trait Hyperbolic<T>
 {
     fn sinh(self) -> T;
@@ -109,9 +117,81 @@ pub trait Hyperbolic<T>
     fn tanh(self) -> T;
 }
 
+/// Trait for inverse hyperbolic functions
 pub trait InverseHyperbolic<T>
 {
     fn asinh(self) -> T;
     fn acosh(self) -> T;
     fn atanh(self) -> T;
+}
+
+#[cfg(test)]
+mod tests 
+{
+    use super::*;
+    use rstest::*;
+    use float_cmp::assert_approx_eq;
+
+    #[rstest]
+    #[case(1.0, std::f64::consts::FRAC_PI_4)]
+    fn test_trig_tan(#[case] v:f64, #[case] r: f64)
+    {
+        let atan = v.atan();
+        assert_approx_eq!(f64, r, atan);
+        let tan = r.tan();
+        assert_approx_eq!(f64, v, tan);
+        let cot = r.cot();
+        assert_approx_eq!(f64, 1.0, tan * cot);
+    }
+
+    #[rstest]
+    #[case(std::f64::consts::PI, 0.0, 1.0)]
+    fn test_trig_sincos(#[case] r: f64, #[case] s:f64, #[case] c:f64)
+    {
+        let sin = r.sin();
+        let cos = r.cos();
+        assert_approx_eq!(f64, s, sin);
+        assert_approx_eq!(f64, c, cos);
+        let (s, c) = r.sincos();
+        assert_approx_eq!(f64, s, sin);
+        assert_approx_eq!(f64, c, cos);
+    }
+
+    #[rstest]
+    #[case(-1.0, -std::f64::consts::FRAC_PI_2)]
+    fn test_trig_asin(#[case] v: f64, #[case] r: f64)
+    {
+        assert_approx_eq!(f64, r, v.asin());
+        let sin = r.sin();
+        assert_approx_eq!(f64, v, sin);
+        let sec = r.sec();
+        assert_approx_eq!(f64, 1.0, sec * sin);
+        let vcos = r.vcos();
+        assert_approx_eq!(f64, 1.0, sin + vcos);
+    }
+
+    #[rstest]
+    #[case( -1.0, std::f64::consts::PI)]
+    fn test_trig_cos(#[case] v: f64, #[case] r: f64)
+    {
+        assert_approx_eq!(f64, r, v.acos());
+        let cos = r.cos();
+        assert_approx_eq!(f64, v, cos);
+        let csc = r.csc();
+        assert_approx_eq!(f64, 1.0, cos * csc);
+        let vsin = r.vsin();
+        assert_approx_eq!(f64, 1.0, cos + vsin);
+    }
+
+    #[rstest]
+    #[case(1.0, 1.0, std::f64::consts::FRAC_PI_4)]
+    fn test_trig_atan2(#[case] y:f64, #[case] x:f64, #[case] r: f64)
+    {
+        let atan2 = f64::atan2(y, x);
+        assert_approx_eq!(f64, r, atan2);
+        let tan = r.tan();
+        assert_approx_eq!(f64, y/x, tan);
+        let cot = r.cot();
+        assert_approx_eq!(f64, 1.0, tan * cot);
+    }
 }
